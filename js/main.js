c1353ba7edc75e29e973cccac458ed4a45e98c99
Jason Melvin Hartono <jason.melvin689@gmail.com>
2023-12-14 22:26:15 +0700

    footer section finished, added some animation to elements, added a sticky navbar, page responsiveness under development

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
 window.onload = function(){
     document.getElementById("homebar").focus();
 }
+
+window.addEventListener("scroll", function(){
+    let navbar = this.document.querySelector("nav");
+    navbar.classList.toggle("sticky", window.scrollY > 85);
+});
