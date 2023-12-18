77133289f43613bd472f5c3873adbf923d291db7
Jason Melvin Hartono <jason.melvin689@gmail.com>
2023-12-18 21:47:16 +0700

    Remove all comments

--- js/main.js
+++ js/main.js
@@ -1,7 +1,3 @@
-// window.onload = function(){
-//     document.getElementById("homebar").focus();
-// }
-
 window.addEventListener("scroll", function(){
     let navbar = this.document.querySelector("nav");
     navbar.classList.toggle("sticky", window.scrollY > 85);
